073ed37a5780b40a3308a2099f16ef592a760429
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-08 18:39:53 +0300

    squash!

--- Source/Function/Redirect.ts
+++ Source/Function/Redirect.ts
@@ -5,7 +5,7 @@
 export default (async (...[URL = "", Status = 302]: Parameters<Type>) =>
 	Response.redirect(URL, Status)) satisfies Type as Type;
 
-import type Type from "../Interface/Redirect.js";
+import type Type from "@Interface/Redirect.js";
 
 export const { Response } = await import(
 	"@cloudflare/workers-types/experimental/index.js"
