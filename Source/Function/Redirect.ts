95ae6af8e5c75d31ed6b7841c83a15c6966954b7
Nikola Hristov <nikola@playform.cloud>
2024-05-07 14:47:37 +0300

    squash!

--- Source/Function/Redirect.ts
+++ Source/Function/Redirect.ts
@@ -5,7 +5,7 @@
 export default (async (...[URL = "", Status = 302]: Parameters<Interface>) =>
 	Response.redirect(URL, Status)) satisfies Interface as Interface;
 
-import type Interface from "@Interface/Redirect.js";
+import type Interface from "../Interface/Redirect.js";
 
 export const { Response } = await import(
 	"@cloudflare/workers-types/experimental/index.js"
